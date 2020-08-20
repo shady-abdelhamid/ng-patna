72e6d1931e4335a2a332d3ee6616d9f77a8db610
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-08-20 06:32:22 +0200

    Add filter functionality

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,77 +1,101 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from "../product.model";
+import { Product } from '../product.model';
 
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[] = [
     {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "rating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+      productId: 1,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      rating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
     },
     {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2019",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "rating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
+      productId: 2,
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2019',
+      description: '15 gallon capacity rolling garden cart',
+      price: 32.99,
+      rating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
     },
     {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2019",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "rating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21, 2019',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      rating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
     },
     {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2019",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "rating": 3.7,
-      "imageUrl": "assets/images/saw.png"
+      productId: 8,
+      productName: 'Saw',
+      productCode: 'TBX-0022',
+      releaseDate: 'May 15, 2019',
+      description: '15-inch steel blade hand saw',
+      price: 11.55,
+      rating: 3.7,
+      imageUrl: 'assets/images/saw.png',
     },
     {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2018",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "rating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
+      productId: 10,
+      productName: 'Video Game Controller',
+      productCode: 'GMG-0042',
+      releaseDate: 'October 15, 2018',
+      description: 'Standard two-button video game controller',
+      price: 35.95,
+      rating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.png',
+    },
   ];
 
   showImage: boolean;
 
-  filter: string = '';
+  filteredProducts: Product[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  _filter: string = '';
+  get filter(): string {
+    return this._filter;
+  }
+  set filter(value: string) {
+    this._filter = value;
+    this.filteredProducts = this.filter
+      ? this.performFilter(this.filter)
+      : this.products;
   }
 
+  constructor() {
+    this.filteredProducts = this.products;
+    this.filter = '';
+  }
+
+  ngOnInit(): void {}
+
+  /**
+   * filter function for products
+   * @param filterBy criteria used to filter products
+   */
+  performFilter(filterBy: string): Product[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product: Product) =>
+          product.productName.toLocaleLowerCase().includes(filterBy));
+  }
+
+  /**
+   * toggle used to show or hide product images
+   */
   toggleImage(): void {
-    this.showImage = ! this.showImage;
+    this.showImage = !this.showImage;
   }
-
 }
