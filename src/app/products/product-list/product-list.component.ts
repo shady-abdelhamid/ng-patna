e5cf6115a76cc30b32d7fc098beb911b20f31e0d
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-08-20 07:25:42 +0200

    Add product service

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,68 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
-
+import { ProductService } from '../product.service';
 @Component({
   selector: 'product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
-  products: Product[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      rating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      rating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2019',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      rating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-    {
-      productId: 8,
-      productName: 'Saw',
-      productCode: 'TBX-0022',
-      releaseDate: 'May 15, 2019',
-      description: '15-inch steel blade hand saw',
-      price: 11.55,
-      rating: 3.7,
-      imageUrl: 'assets/images/saw.png',
-    },
-    {
-      productId: 10,
-      productName: 'Video Game Controller',
-      productCode: 'GMG-0042',
-      releaseDate: 'October 15, 2018',
-      description: 'Standard two-button video game controller',
-      price: 35.95,
-      rating: 4.6,
-      imageUrl: 'assets/images/xbox-controller.png',
-    },
-  ];
-
   showImage: boolean;
 
-  filteredProducts: Product[];
+  filteredProducts: Product[] = [];
+  products: Product[] = [];
 
   _filter: string = '';
   get filter(): string {
@@ -75,12 +23,14 @@ export class ProductListComponent implements OnInit {
       : this.products;
   }
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.filter = '';
+  constructor(private productService: ProductService) {
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+  }
 
   /**
    * filter function for products
@@ -89,7 +39,8 @@ export class ProductListComponent implements OnInit {
   performFilter(filterBy: string): Product[] {
     filterBy = filterBy.toLowerCase();
     return this.products.filter((product: Product) =>
-          product.productName.toLocaleLowerCase().includes(filterBy));
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
   }
 
   /**
