55274bf740bd5fdbaf2de5b69f96896b4ec0d2cb
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-08-22 00:28:08 +0200

    Add welcome and not found pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './core/welcome/welcome.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
-const routes: Routes = [{ path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'products', loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule)},
+  { path: '**', component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
