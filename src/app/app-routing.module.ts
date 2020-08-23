d1daa4c544a5cff38f4884c01f383ff082ac23b1
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-08-23 08:49:20 +0200

    Add user settings module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'products', loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule)},
+  { path: 'user-settings', loadChildren: () => import('./user-settings/user-settings.module').then(m => m.UserSettingsModule) },
   { path: '**', component: PageNotFoundComponent }
 
 ];
