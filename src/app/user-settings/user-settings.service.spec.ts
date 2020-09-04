05c90c242140322ab5024065382e50aa69b09502
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-04 23:11:21 +0200

    fix spec

--- src/app/user-settings/user-settings.service.spec.ts
+++ src/app/user-settings/user-settings.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserSettingsService } from './user-settings.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UserSettingsService', () => {
   let service: UserSettingsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UserSettingsService);
   });
 
