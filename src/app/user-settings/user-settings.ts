4d5f7c821c2030e3bac605f7f11b2bdab0c16202
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-04 23:05:39 +0200

    fix lint

--- src/app/user-settings/user-settings.ts
+++ src/app/user-settings/user-settings.ts
@@ -1,14 +1,14 @@
-import { SubscriptionType } from "./subscription-type.enum";
-import { InterfaceStyle } from "./interface-style.enum";
+import { SubscriptionType } from './subscription-type.enum';
+import { InterfaceStyle } from './interface-style.enum';
 
 /**
  * User settings domain model
  */
 export interface UserSettings {
-  name: string,
-  emailOffers: boolean,
-  interfaceStyle: InterfaceStyle,
-  subscriptionType: SubscriptionType,
-  notes: string,
-  password: string,
+  name: string;
+  emailOffers: boolean;
+  interfaceStyle: InterfaceStyle;
+  subscriptionType: SubscriptionType;
+  notes: string;
+  password: string;
 }
