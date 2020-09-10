540b64efab8a53f403bea5ac08c94fc7682cf46e
shady-abdelhamid <shady.abdelhamid@outlook.com>
2020-09-10 19:59:04 +0200

    Add edit component

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -4,18 +4,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductDetailGuard } from './product-detail/product-detail.guard';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   {
     path: ':id',
     canActivate: [ProductDetailGuard],
-    component: ProductDetailComponent
-  }
+    component: ProductDetailComponent,
+  },
+  {
+    path: ':id/edit',
+    canActivate: [ProductDetailGuard],
+    component: ProductEditComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,10 +6,11 @@ import { SharedModule } from '../shared/shared.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 
 @NgModule({
-  declarations: [ProductListComponent, ProductDetailComponent],
+  declarations: [ProductListComponent, ProductDetailComponent, ProductEditComponent],
   imports: [
     SharedModule,
     FormsModule,
